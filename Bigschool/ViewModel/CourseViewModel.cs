211f4666ac277c925a51a660021580af72a48614
hiep <hohiep5566@gmail>
2019-04-04 11:10:49 +0700

    lam them

--- Bigschool/ViewModel/CourseViewModel.cs
+++ Bigschool/ViewModel/CourseViewModel.cs
@@ -15,6 +15,7 @@ namespace Bigschool.ViewModel
         [FutureDate]
         public string Date { get; set; }
         [Required]
+        [ValidTime]
         public string Time { get; set; } 
         [Required]   
         public byte Category { get; set; }
